92f3a9b4803ca959d9b5f7dffde9d3e77c7f2be9
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-08-03 16:19:28 +0200

    Added main links

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,4 +23,40 @@ $comics= config("comics");
             <button>LOAD MORE</button>
         </div>
     </div>
+    <div class="main-links">
+        <div class="container">
+            <ul>
+                <li>
+                    <a href="#">
+                        <img src="{{Vite::asset("resources/img/buy-comics-digital-comics.png")}}" alt="DC">
+                        <h3>DIGITAL COMICS</h3>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="{{Vite::asset("resources/img/buy-comics-merchandise.png")}}" alt="DC">
+                        <h3>DC MERCHANDISE</h3>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="{{Vite::asset("resources/img/buy-comics-subscriptions.png")}}" alt="DC">
+                        <h3>SUBSCRIPTION</h3>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="{{Vite::asset("resources/img/buy-comics-shop-locator.png")}}" alt="DC">
+                        <h3>COMIC SHOP LOCATOR</h3>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="{{Vite::asset("resources/img/buy-dc-power-visa.svg")}}" alt="DC">
+                        <h3>DC POWER VISA</h3>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
 @endsection
