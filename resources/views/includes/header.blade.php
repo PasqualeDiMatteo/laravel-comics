45578a1411794d9b83fafdd95d7d5d2c8e2e9afe
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-08-03 15:27:15 +0200

    Added Header

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,23 @@
+<header>
+    <div class="container">
+        <div class="nav-bar">
+            <figure>
+                <a href="#"><img src="{{Vite::asset("resources/img/dc-logo.png")}}" alt="DC Logo"></a>
+            </figure>
+            <nav>
+                <ul>
+                    <li><a href="#">CARACTERS</a></li>
+                    <li><a href="#">COMICS</a></li>
+                    <li><a href="#">MOVIES</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">GAMES</a></li>
+                    <li><a href="#">COLLECTIBLES</a></li>
+                    <li><a href="#">VIDEOS</a></li>
+                    <li><a href="#">FANS</a></li>
+                    <li><a href="#">NEWS</a></li>
+                    <li><a href="#">SHOP</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
