888da9195000405a52c887b7d3b72b512d331619
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-08-03 16:00:14 +0200

    Added Main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,26 @@
 @extends('layout.main')
 
 @section('title',"Comics")
+
+@php
+$comics= config("comics");    
+@endphp
+
+@section('main-content')
+    {{-- Background --}}
+    <div class="background">
+        <div class="container container-card">
+            <div class="series">CURRENT SERIES</div>
+            <div class="content">
+                {{-- Card --}}
+                @foreach ($comics as $comic)
+                <div class="card">
+                    <img src="{{$comic["thumb"]}}" alt='{{ $comic["series"] }}'>
+                    <h3>{{ $comic["series"] }}</h3>
+                </div>
+                @endforeach
+            </div>
+            <button>LOAD MORE</button>
+        </div>
+    </div>
+@endsection
