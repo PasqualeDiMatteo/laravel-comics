7f6f090ec0a9a605cfda0bd8c4d9c9665c97b5c5
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-08-03 17:27:44 +0200

    added comments

--- routes/web.php
+++ routes/web.php
@@ -13,46 +13,67 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/comics', function () {
-    return view('comics');
-})->name("comics");
+// Home
 
 Route::get('/', function () {
     return view('home');
 })->name("home");
 
+// Caracters
+
 Route::get('/caracters', function () {
     return view('caracters');
 })->name("caracters");
+
+// Comics
+
+Route::get('/comics', function () {
+    return view('comics');
+})->name("comics");
+
+// Movies
 
 Route::get('/movies', function () {
     return view('movies');
 })->name("movies");
 
+// Tv
+
 Route::get('/tv', function () {
     return view('tv');
 })->name("tv");
 
+// Games
 
 Route::get('/games', function () {
     return view('games');
 })->name("games");
 
+// Collectibles
+
 Route::get('/collectibles', function () {
     return view('collectibles');
 })->name("collectibles");
+
+// Videos
 
 Route::get('/videos', function () {
     return view('videos');
 })->name("videos");
 
+// Fans
+
 Route::get('/fans', function () {
     return view('fans');
 })->name("fans");
 
+// News
+
 Route::get('/news', function () {
     return view('news');
 })->name("news");
+
+// Shop
 
 Route::get('/shop', function () {
     return view('shop');
